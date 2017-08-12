ded8702888688473cc94236a7691af8a59fc05d1
Rob Latimer <rtlatimer@gmail.com>
2017-08-11 22:27:21 -0700

    style: Add comments to problem 1.9

--- Chapter 1/1_9_StringRotation.py
+++ Chapter 1/1_9_StringRotation.py
@@ -13,14 +13,14 @@ from collections import Counter
 
 def isSubstring(s1,s2):
 	print "Looking for '{}'' in '{}': ".format(s2, s1)
-	if len(s1) != len(s2):
+	if len(s1) != len(s2): # make sure strings are same length
 		return False
-	s1_dict = Counter(s1)
+	s1_dict = Counter(s1) # create dictionary counts of each letter to compare
 	s2_dict = Counter(s2)
-	if s1_dict != s2_dict:
+	if s1_dict != s2_dict: # each word should have the same letters, just in a different order
 		return False
 	for i in range(len(s1)):
-		if s2[i:] + s2[:i] == s1:
+		if s1[i:] + s1[:i] == s2: # checks all rotations of s1
 			return True
 	return False
 
